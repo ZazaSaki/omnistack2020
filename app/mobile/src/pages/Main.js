aa0fc4293d1e34270645655e2dfa9be2064b36ee
Zaza Saki <isac.cruzn@gmail.com>
2020-01-22 13:36:08 +0000

    Safe: Expo pkg, and dev; React Navigation; File structre ==> (mobile)

---
+++ app/mobile/src/pages/Main.js
@@ -0,0 +1,8 @@
+import React, { version } from 'react';
+import {View} from 'react-native';
+
+function Main(){
+    return <View/>
+}
+
+export default Main;
---
+++ app/mobile/src/pages/Profile.js
@@ -0,0 +1,8 @@
+import React, { version } from 'react';
+import {View} from 'react-native';
+
+function Profile(){
+    return <View/>
+}
+
+export default Profile;
